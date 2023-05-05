7f93fa53c47633bcaa6b9f1d06bffb2856ec2b23
MasterMousa <mousshamed4@gmail.com>
2023-05-05 02:03:23 +0200

    #FIX: margin top in mobile main page links

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -23,7 +23,7 @@ export default component$(() => {
           <Link
             key={index}
             href={link.url}
-            class="sm:basis-[calc(100%/4-1rem)] basis-full h-fit flex flex-col items-center border-2 border-gray-900 hover:border-gray-500 hover:bg-gray-50 mt-2 hover:mt-0 rounded-3xl"
+            class="sm:basis-[calc(100%/4-1rem)] basis-full h-fit flex flex-col items-center border-2 border-gray-900 hover:border-gray-500 hover:bg-gray-50 mt-4 hover:mt-2 rounded-3xl"
           >
             <p class="my-4 font-poppins">{link.title}</p>
             <img
