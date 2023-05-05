2d84b550e0a99b5f246d19f29fd3e1986fc54513
MasterMousa <mousshamed4@gmail.com>
2023-05-05 19:47:36 +0200

    Update QR Code Component design

--- src/components/qr-code-component-main/index.tsx
+++ src/components/qr-code-component-main/index.tsx
@@ -6,10 +6,10 @@ export default component$(() => {
     <main class="w-screen h-screen flex justify-center items-center bg-[hsl(212,45%,89%)] text-center">
       <section class="xs:w-[330px] w-[85%] flex flex-col bg-white p-4 rounded-3xl">
         <img class="rounded-xl" src={QRCodeImage} alt="QR Code Image" />
-        <h1 class="my-6 mx-2 xs:text-2xl text-xl text-[hsl(218,44%,22%)] font-bold font-outfit">
+        <h1 class="mt-6 mb-4 mx-2 xs:text-2xl text-xl text-[hsl(218,44%,22%)] font-bold font-outfit">
           Improve your front-end skills by building projects
         </h1>
-        <p class="mx-2 font-outfit text-[hsl(220,15%,55%)] leading-tight">
+        <p class="mx-2 mb-4 font-outfit text-[hsl(220,15%,55%)] leading-tight">
           Scan the QR code to visit Frontend Mentor and take your coding skills
           to the next level
         </p>
